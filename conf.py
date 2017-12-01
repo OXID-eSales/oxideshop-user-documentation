36865b828b62b8f729e95a31a246bed7cb154877
Jürgen Busch <juergen.jb.busch@gmail.com>
2017-12-01 15:42:45 +0100

    Corrected logo entry for html output

--- conf.py
+++ conf.py
@@ -171,7 +171,7 @@ html_title = 'OXID eShop 4&5 | Anwenderdokumentation'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = '_static/img/logo.png'
+html_logo = '_themes/sphinx_rtd_theme/static/logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
