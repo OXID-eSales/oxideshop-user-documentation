f93bd87903486296f9321253eb20f4d88d5f74a0
Jürgen Busch <juergen.jb.busch@gmail.com>
2017-12-19 14:04:14 +0100

    Replaced logo and added favicon

--- conf.py
+++ conf.py
@@ -169,7 +169,7 @@ html_logo = '_themes/sphinx_rtd_theme/static/logo.png'
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-html_favicon = None
+html_favicon = '_themes/sphinx_rtd_theme/static/favicon.gif'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
