b1ef84d65918ed71bbf12bb2dcaf1f5abbd73da5
Juergen-Busch <juergen.busch@oxid-esales.com>
2020-04-27 10:08:47 +0200

    Added :doc:`OXID eShop 6.2.1 <oxid-eshop-621>`

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'OXID eSales AG'
 # The short X.Y version
 version = '6.2'
 # The full version, including alpha/beta/rc tags
-release = '6.2.0'
+release = '6.2.1'
 
 # -- General configuration ---------------------------------------------------
 
--- releases/releases-2020/index.rst
+++ releases/releases-2020/index.rst
@@ -4,4 +4,5 @@ Releases 2020
 .. toctree::
    :maxdepth: 1
 
+   28.04.2020 - OXID eShop 6.2.1 <oxid-eshop-621>
    31.03.2020 - OXID eShop 6.2.0 <oxid-eshop-620>
