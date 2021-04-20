e048427c79f7c4df0470d5b9d457a55cc8ba7586
Juergen-Busch <juergen.busch@oxid-esales.com>
2021-04-20 10:37:57 +0200

    Adapted version switch

--- conf.py
+++ conf.py
@@ -139,9 +139,7 @@ html_context = {
     'current_version': '5.3',
     'versions':
             [('5.3', 'https://docs.oxid-esales.com/eshop/de/5.3/'),
-             ('6.0', 'https://docs.oxid-esales.com/eshop/de/6.0/'),
-             ('6.1', 'https://docs.oxid-esales.com/eshop/de/6.1/'),
-             ('6.2', 'https://docs.oxid-esales.com/eshop/de/6.2/')],
+             ('Aktuell', 'https://docs.oxid-esales.com/eshop/de/latest/')],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
     'show_sphinx': False,
