6d52499c6fb1543445d17952e8755caa5adbacb8
christoph albrecht <christoph.albrecht@oxid-esales.com>
2022-08-17 15:20:16 +0200

    Version switch adjusted -- de

--- conf.py
+++ conf.py
@@ -85,7 +85,8 @@ html_context = {
         [('6.1', 'https://docs.oxid-esales.com/eshop/de/6.1/'),
          ('6.2', 'https://docs.oxid-esales.com/eshop/de/6.2/'),
          ('6.3', 'https://docs.oxid-esales.com/eshop/de/6.3/'),
-         ('6.4', 'https://docs.oxid-esales.com/eshop/de/6.4/')],
+         ('6.4', 'https://docs.oxid-esales.com/eshop/de/6.4/'),
+         ('6.5', 'https://docs.oxid-esales.com/eshop/de/6.5/')],
     'languages':
         [('DE', 'https://docs.oxid-esales.com/eshop/de/6.2/'),
          ('EN', 'https://docs.oxid-esales.com/eshop/en/6.2/')],
