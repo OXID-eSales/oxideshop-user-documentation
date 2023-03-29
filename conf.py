6fb02a0cd78a57510271dd4de3c30a9b8ab2237f
christoph albrecht <christoph.albrecht@oxid-esales.com>
2023-03-29 16:49:34 +0200

    preliminary review version created -- DE

--- conf.py
+++ conf.py
@@ -85,13 +85,13 @@ pygments_style = None
 html_theme = 'sphinx_rtd_theme'
 
 html_context = {
-    'current_version': '7.0',
+    'current_version': '7.0-prelim',
     'versions':
         [('6.2', 'https://docs.oxid-esales.com/eshop/de/6.2/'),
          ('6.3', 'https://docs.oxid-esales.com/eshop/de/6.3/'),
          ('6.4', 'https://docs.oxid-esales.com/eshop/de/6.4/'),
          ('6.5', 'https://docs.oxid-esales.com/eshop/de/6.5/'),
-         ('7.0', 'https://docs.oxid-esales.com/eshop/de/7.0/')],
+         ('7.0', 'https://docs.oxid-esales.com/eshop/de/7.0-prelim/')],
     'languages':
         [('DE', 'https://docs.oxid-esales.com/eshop/de/7.0/'),
          ('EN', 'https://docs.oxid-esales.com/eshop/en/7.0/')],
@@ -103,7 +103,7 @@ html_context = {
     # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
     'github_user': 'OXID-eSales',
     'github_repo': 'oxideshop-user-documentation',
-    'github_version': 'b-7.0-de/'
+    'github_version': 'b-7.0-de-700/'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
