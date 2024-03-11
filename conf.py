1ae5ceef181ac5ca363aa132e603a720d74e83fc
christoph albrecht <christoph.albrecht@oxid-esales.com>
2024-03-11 09:37:54 +0100

    6.0, 6.1 restored in navigation -- DE

--- conf.py
+++ conf.py
@@ -87,7 +87,9 @@ html_theme = 'sphinx_rtd_theme'
 html_context = {
     'current_version': '7.1',
     'versions':
-        [('6.2', 'https://docs.oxid-esales.com/eshop/de/6.2/'),
+        [('6.0', 'https://docs.oxid-esales.com/eshop/de/6.0/'),
+         ('6.1', 'https://docs.oxid-esales.com/eshop/de/6.1/'),
+         ('6.2', 'https://docs.oxid-esales.com/eshop/de/6.2/'),
          ('6.3', 'https://docs.oxid-esales.com/eshop/de/6.3/'),
          ('6.4', 'https://docs.oxid-esales.com/eshop/de/6.4/'),
          ('6.5', 'https://docs.oxid-esales.com/eshop/de/6.5/'),
