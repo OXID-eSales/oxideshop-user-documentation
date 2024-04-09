7028e606e45d3fda5f12ad93582aecddb4bfa4c8
christoph albrecht <christoph.albrecht@oxid-esales.com>
2024-04-09 16:34:49 +0200

    710: version switch adjusted -- DE

--- conf.py
+++ conf.py
@@ -137,6 +137,7 @@ html_context = {
          ('6.4', 'https://docs.oxid-esales.com/eshop/de/6.4/'),
          ('6.5', 'https://docs.oxid-esales.com/eshop/de/6.5/'),
          ('7.0', 'https://docs.oxid-esales.com/eshop/de/7.0/'),
+         ('7.1', 'https://docs.oxid-esales.com/eshop/de/7.1/'),
          ],
     'languages':
         [('DE', 'https://docs.oxid-esales.com/eshop/de/6.1/'),
