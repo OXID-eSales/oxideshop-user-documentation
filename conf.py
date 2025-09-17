ebbf4f1f8eacd07e387f8ebd274cef9b3ff57453
christoph albrecht <christoph.albrecht@oxid-esales.com>
2025-09-17 11:40:52 +0200

    OXDEV-8935: 7.4 initial

--- conf.py
+++ conf.py
@@ -20,14 +20,14 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'OXID eShop 7.3'
-copyright = '2003 - {}, OXID eSales AG'.format(dt.date.today().year)
+project = 'OXID eShop 7.4'
+copyright = '2003 \u2013 {}, OXID eSales AG'.format(dt.date.today().year)
 author = 'OXID eSales AG'
 
 # The short X.Y version
-version = '7.3'
+version = '7.4'
 # The full version, including alpha/beta/rc tags
-release = '7.3.0'
+release = '7.4.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -85,7 +85,7 @@ pygments_style = None
 html_theme = 'sphinx_rtd_theme'
 
 html_context = {
-    'current_version': '7.3',
+    'current_version': '7.4',
     'versions': [
          ('6.0', 'https://docs.oxid-esales.com/eshop/de/6.0/'),
          ('6.1', 'https://docs.oxid-esales.com/eshop/de/6.1/'),
@@ -97,10 +97,11 @@ html_context = {
          ('7.1', 'https://docs.oxid-esales.com/eshop/de/7.1/'),
          ('7.2', 'https://docs.oxid-esales.com/eshop/de/7.2/'),
          ('7.3', 'https://docs.oxid-esales.com/eshop/de/7.3/'),
+         ('7.4', 'https://docs.oxid-esales.com/eshop/de/7.4/'),
          ],
     'languages':
-        [('DE', 'https://docs.oxid-esales.com/eshop/de/7.3/'),
-         ('EN', 'https://docs.oxid-esales.com/eshop/en/7.3/')],
+        [('DE', 'https://docs.oxid-esales.com/eshop/de/7.4/'),
+         ('EN', 'https://docs.oxid-esales.com/eshop/en/7.4/')],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
     'show_sphinx': False,
@@ -109,7 +110,7 @@ html_context = {
     # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
     'github_user': 'OXID-eSales',
     'github_repo': 'oxideshop-user-documentation',
-    'github_version': 'b-7.3.x-de/'
+    'github_version': 'b-7.4.x-de/'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -124,7 +125,7 @@ html_theme_path = ['_themes']
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-html_title = 'OXID eShop 7.3 | Anwenderdokumentation'
+html_title = 'OXID eShop 7.4 | Anwenderdokumentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
@@ -211,7 +212,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'OXIDeShop70.tex', 'OXID eShop 7.3 Anwenderdokumentation',
+    (master_doc, 'OXIDeShop70.tex', 'OXID eShop 7.4 Anwenderdokumentation',
      'OXID eSales AG', 'manual'),
 ]
 
@@ -221,7 +222,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'oxideshop70', 'OXID eShop 7.3 Anwenderdokumentation',
+    (master_doc, 'oxideshop70', 'OXID eShop 7.4 Anwenderdokumentation',
      [author], 1)
 ]
 
@@ -232,7 +233,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'OXIDeShop70', 'OXID eShop 7.3 Anwenderdokumentation',
+    (master_doc, 'OXIDeShop70', 'OXID eShop 7.4 Anwenderdokumentation',
      author, 'OXIDeShop70', 'One line description of project.',
      'Miscellaneous'),
 ]
