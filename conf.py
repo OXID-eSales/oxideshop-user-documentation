9dabb7c86d017a91f4c161eeada4030c87b704cb
christoph albrecht <christoph.albrecht@oxid-esales.com>
2025-11-27 11:35:09 +0100

    OXDEV-9725: Copyright date readjusted

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'OXID eShop 6.1'
-copyright = '{}, OXID eSales AG'.format(dt.date.today().year)
+copyright = '2003 \u2013 {}, OXID eSales AG'.format(dt.date.today().year)
 author = 'OXID eSales AG'
 
 # The version info for the project you're documenting, acts as replacement for
